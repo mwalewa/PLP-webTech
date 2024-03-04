f17d42937908f4d2f37724c26c82fd175bdfeb56
mwalewa <amwalewa@gmail.com>
2024-03-04 20:40:14 +0300

    First Commit

---
+++ .dart/variables.dart
@@ -0,0 +1,15 @@
+void main(){
+  //declaring variables
+  String name = "Mwalewa";
+  String address = "Kenya";
+  num age = 35;  
+  num height = 5.8;
+  bool ismarried = true;
+
+//printing variables value
+print ("Name is $name");
+print ("Address is $address");
+print ("Age is $age");
+print ("Height is $height");
+print ("Married status is $ismarried");
+}
